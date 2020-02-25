6df136479224c12f8eb9c8e8a4e873a233e2d360
Oleh Hanyak <ohanyak@gmail.com>
2020-02-25 18:03:39 +0200

    add

---
+++ lesson_4/hw2/index.js
@@ -0,0 +1,23 @@
+const m = 5;
+const n = 35;
+let result = 0;
+
+for (let i = 0; i <= n; i++) {
+    if (i % 2 === 0 && i % 4 === 0) {
+        result += i;
+    }
+    else if (i % 3 === 0) {
+        result -= i;
+    }
+    else if (i % 4 === 0) {
+        result *= i;
+    }
+    else if (i % 5 === 0) {
+        console.log(i);
+    }
+    else {
+        continue;
+    }
+}
+
+// console.log(result);
