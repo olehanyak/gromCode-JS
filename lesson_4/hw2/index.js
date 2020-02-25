367a764a3cf7580f9f20d90524696394cf301d09
Oleh Hanyak <ohanyak@gmail.com>
2020-02-25 18:27:40 +0200

    up

--- lesson_4/hw2/index.js
+++ lesson_4/hw2/index.js
@@ -2,18 +2,18 @@ const m = 7;
 const n = 42;
 let result = 0;
 
-for (let m = 7; m <= n; m++) {
-    if (m % 2 === 0 && m % 4 === 0) {
-        result += m;
+for (let i = m; i <= n; i++) {
+    if (i % 2 === 0 && i % 4 === 0) {
+        result += i;
     }
-    else if (m % 3 === 0) {
-        result -= m;
+    else if (i % 3 === 0) {
+        result -= i;
     }
-    else if (m % 4 === 0) {
-        result *= m;
+    else if (i % 4 === 0) {
+        result *= i;
     }
-    else if (m % 5 === 0) {
-        console.log(m);
+    else if (i % 5 === 0) {
+        console.log(i);
     }
     else {
         continue;
