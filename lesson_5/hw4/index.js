d475fb6b93e0fc82abf0deef4fd34cdbbceb30c4
Oleh Hanyak <ohanyak@gmail.com>
2020-02-27 16:21:04 +0200

    add

--- lesson_5/hw4/index.js
+++ lesson_5/hw4/index.js
@@ -1,19 +1,26 @@
+function sum(from, to) {
+    let getSum = 0;
+    for (let i = from; i <= to; i++) {
+        console.log(getSum);
+        getSum += i;
+    }
+    return getSum;
+}
 
+// sum(1, 5);
 
 function compareSums(a, b, c, d) {
-    let sum1 = a + b;
-    let sum2 = c + d;
-    function sum(from, to) {
-        if (from > to) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    // let sum1 = (a, b);
+    // let sum2 = (c, d);
+    if (getSum(a, b) > getSum(c, d)) {
+        console.log(getSum(a, b));
+        return true;
     }
-    sum(sum1, sum2);
+    else {
+        return false;
+    }
+    // sum(sum1, sum2);
 }
 
 
-compareSums(10, 3, 4, 1);
-
+// compareSums(2, 5, 3, 10);
