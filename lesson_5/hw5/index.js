ea06bcab3e61c54ce73f3d3b0f1b6f4750794391
Oleh Hanyak <ohanyak@gmail.com>
2020-02-27 17:35:17 +0200

    count

--- lesson_5/hw5/index.js
+++ lesson_5/hw5/index.js
@@ -2,10 +2,14 @@ function findDivCount(a, b, n) {
     let sum = 0;
     for (let i = a; i <= b; i++) {
         if (i % n === 0) {
-            sum += i;
+            // sum += i;
+            sum++;
+            // console.log(sum.length);
+            // console.log(sum);
+            // sum += i;
         }
     }
-    console.log(sum);
+    // console.log(sum);
     return sum;
 }
 
