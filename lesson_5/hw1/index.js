821590503b91a20251d32f3f32e2b740b2d56b49
Oleh Hanyak <ohanyak@gmail.com>
2020-02-27 13:49:27 +0200

    add

--- lesson_5/hw1/index.js
+++ lesson_5/hw1/index.js
@@ -1,14 +1,13 @@
 function getSum(num1, num2) {
     let sum = 0;
     for (let i = num1; i <= num2; i++) {
-        sum += i;
-        console.log(sum);
         if (i % 2 === 0) {
             sum += i;
             console.log(sum);
-            return sum;
         }
     }
+    console.log(sum);
+    return sum;
 }
 
-getSum(1, 10);
+getSum(3, 10);
