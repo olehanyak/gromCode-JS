a8e84cd7588c46d82dd632c023aba3d4eb8804a6
Oleh Hanyak <ohanyak@gmail.com>
2020-02-27 18:35:27 +0200

    primes_func

---
+++ lesson_5/hw2/index.js
@@ -0,0 +1,18 @@
+
+function getPrimes(num) {
+    // if (num === 1) {
+    //     return false;
+    // }
+    next: 
+    for (let i = 2; i <= num; i++) {
+        for (let k = 2; k < i; k++) {
+            if (i % k === 0) {
+                continue next;
+            }
+        }
+        console.log(i);
+    }
+}
+
+getPrimes(30);
+// console.log(getPrimes());
