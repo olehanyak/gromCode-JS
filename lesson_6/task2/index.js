c24812b3360e887dea15d64fc1ebea197bb42d61
Oleh Hanyak <ohanyak@gmail.com>
2020-02-28 09:01:40 +0200

    add

--- lesson_6/task2/index.js
+++ lesson_6/task2/index.js
@@ -1,15 +1,16 @@
 const arrayNumbers = [5, 7, 9, 16];
 
 function getSum(arr) {
+
     let sum = 0;
-    for (let i = 0; i < arrayNumbers.length; i++) {
-        sum += arrayNumbers[i];
-        console.log(sum);
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
 
         if (!Array.isArray(arr)) {
             return null;
         }
     }
+    console.log(sum)
     return sum;
 }
 
