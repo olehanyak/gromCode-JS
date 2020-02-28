ae01a679ea3341f7c6ad647ae483bd2b9e8ab583
Oleh Hanyak <ohanyak@gmail.com>
2020-02-28 13:28:21 +0200

    add

--- lesson_6/hw2/index.js
+++ lesson_6/hw2/index.js
@@ -1,12 +1,16 @@
 const mainArray = [1, 2, 3, 4, 5, 6];
 
 function reverseArray(arr) {
-    const reversed = arr.reverse();
+    // const reversed = arr.reverse();
+    let res = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        res.push(arr[i]);
 
-    if (!Array.isArray(arr)) {
-        return null;
+        if (!Array.isArray(arr)) {
+            return null;
+        }
     }
-    return reversed;
+    return res;
 }
 
 const getReverseArray = reverseArray(mainArray);
