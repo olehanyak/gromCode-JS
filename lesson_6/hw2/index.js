1dd8f998af510bc3aab5c9a79129ea6ce8853a92
Oleh Hanyak <ohanyak@gmail.com>
2020-02-28 13:11:50 +0200

    up

--- lesson_6/hw2/index.js
+++ lesson_6/hw2/index.js
@@ -3,6 +3,10 @@ const mainArray = [1, 2, 3, 4, 5, 6];
 function reverseArray(arr) {
     const [a, b, c, d, e, f] = arr;
 
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
     return [f, e, d, c, b, a];
 }
 
