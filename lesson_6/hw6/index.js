c0520b15ee77ee2fc3d387baa4918b51a7d9f19b
Oleh Hanyak <ohanyak@gmail.com>
2020-03-03 17:07:26 +0200

    add

--- lesson_6/hw6/index.js
+++ lesson_6/hw6/index.js
@@ -1,19 +1,19 @@
-const nameClients = ["Nick", "Oscar", "Mark", "Kate"];
+// const nameClients = ["Nick", "Oscar", "Mark", "Kate"];
 
-const clientBalance = [100, 200, 50, 1000];
+// const clientBalance = [100, 200, 50, 1000];
 
 function withdraw(clients, balances, client, amount) {
     for (let i = 0; i < clients.length; i++) {
         console.log(balances[i]);
         // console.log(client[i])
         if (clients[i] === client && balances[i] > amount) {
-            return (balances[i] -= amount);
+            return balances[i] -= amount;
         } else {
             return -1;
         }
     }
 }
 
-const clientsBalances = withdraw(nameClients, clientBalance, nameClients, clientBalance);
+// const clientsBalances = withdraw(nameClients, clientBalance, nameClients, 1);
 
-console.log(clientsBalances);
+// console.log(clientsBalances);
