8ea9e40d7f71271f3a8d176b57fe57bc5e341a5f
Oleh Hanyak <ohanyak@gmail.com>
2020-03-03 15:03:54 +0200

    update

--- lesson_6/hw6/index.js
+++ lesson_6/hw6/index.js
@@ -3,24 +3,25 @@ const nameClients = ["Nick", "Oscar", "Mark", "Kate"];
 const clientBalance = [100, 200, 50, 1000];
 
 function withdraw(clients, balances, client, amount) {
-    let a = [];
-    let b = [];
+    // let a = [];
+    // let b = [];
     
     for (let i = 0; i < clients.length; i++) {
-        a.push(clients[i]);
-        console.log(a)
+        // a.push(clients[i]);
         console.log(clients[i]);
+        for (let k = 0; k < balances.length; k++) {
+            // b.push(balances[k]);
+            console.log(balances[k]);
+            if (clients[i] === client && balances[k] > amount) {
+                return a -= amount;
+            } 
+            else if (balances[k] < amount) {
+                return -1;
+            }
+        }
+       
     }
-    for (let k = 0; k < balances.length; k++) {
-        b.push(balances[k]);
-        console.log(balances[k]);
-    }
-    if (a === client && b > amount) {
-        return a -= amount;
-    } 
-    else if (b < amount) {
-        return -1;
-    }
+  
 }
 
 const clientsBalances = withdraw(nameClients, clientBalance);
