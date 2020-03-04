9e8fb16b25a491573c105839b2802a0925ee0976
Oleh Hanyak <ohanyak@gmail.com>
2020-03-04 10:30:11 +0200

    hw1

---
+++ lessons7/hw1/index.js
@@ -0,0 +1,18 @@
+const squareArray = numbers => {
+    let squareNum = [];
+
+    numbers.forEach(element => {
+        // console.log(numbers)
+        if (Array.isArray(element)) {
+            return null;
+        }
+        else if (element) {
+            squareNum.push(element * element);
+        }
+    });
+    return squareNum;
+}
+
+const squareNumbers = [2, 3, 4, 5, 6];
+
+console.log(squareArray(squareNumbers));
