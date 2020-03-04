168d7b64b37a2746c3291da27bb0a62a3398610b
Oleh Hanyak <ohanyak@gmail.com>
2020-03-04 07:26:53 +0200

    add

--- lessons7/task2/index.js
+++ lessons7/task2/index.js
@@ -1,15 +1,11 @@
 const sortDesc = numbers => {
-    function compare(a, b) {
-        if (a > b) {
-            return 1;
-        } else {
-            return -1;
-        }
-    }
-    numbers.sort(compare);
-    return numbers;
+  const copy = numbers.slice();
+  copy.sort((a, b) => b - a);
+
+  return copy;
 }
 
 const arr = [1, 3, 66, 7, 2];
 
 console.log(sortDesc(arr));
+console.log(arr);
