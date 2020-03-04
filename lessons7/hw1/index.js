a2fc4e00ada0a56f63529879a0d9eeebc5f88f6b
Oleh Hanyak <ohanyak@gmail.com>
2020-03-04 16:07:53 +0200

    add

--- lessons7/hw1/index.js
+++ lessons7/hw1/index.js
@@ -2,8 +2,8 @@ const squareArray = numbers => {
     if (!Array.isArray(numbers)) {
         return null;
     }
-    const a = numbers.map(elem => elem * 2);
-    return a;
+    const squareNum = numbers.map(elem => elem * elem);
+    return squareNum;
 }
 
 
@@ -11,4 +11,4 @@ const squareNumbers = [2, 3, 4, 5, 6];
 
 console.log(squareArray(squareNumbers));
 
-console.log(squareNumbers);
+// console.log(squareNumbers);
