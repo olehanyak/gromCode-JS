8276cc0b8319190cc6f408accf74a5419e98525a
Oleh Hanyak <ohanyak@gmail.com>
2020-03-05 09:29:10 +0200

    add

---
+++ lesson_8/hw2/index.js
@@ -0,0 +1,22 @@
+const getAdults = obj => {
+    let a = {};
+    for (let key in obj) {
+        // console.log(name)
+        if (obj[key] >= 18) {
+            // console.log(obj)
+            a[[key]] = obj[key];
+        }
+    }
+    return a;
+}
+
+const names = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18,
+};
+
+const result = getAdults(names);
+
+console.log(result);
+// console.log(names);
