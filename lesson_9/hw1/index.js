76d62c166ce05f5b85882bf0cbdf08cba917cb97
Oleh Hanyak <ohanyak@gmail.com>
2020-03-06 14:15:23 +0200

    add

---
+++ lesson_9/hw1/index.js
@@ -0,0 +1,22 @@
+const customers = {
+    "customer-id-1": {
+        name: "William",
+        age: 54
+    },
+    "customer-id-2": {
+        name: "Tom",
+        age: 17
+    }
+};
+
+const getCustomersList = obj => Object.entries(obj)
+    .map(elem => ({ id: elem[0], ...obj[elem[0]] }))
+    .sort((a, b) => a.age - b.age);
+
+// const getCustomersList = obj => Object.entries(obj).forEach([key, value]);
+
+console.log(getCustomersList(customers));
+console.log(Object.values(customers));
+
+
+
