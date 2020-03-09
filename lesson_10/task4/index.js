6e592f136203c3f550964e479743c884518e461d
Oleh Hanyak <ohanyak@gmail.com>
2020-03-09 19:59:53 +0200

    max

--- lesson_10/task4/index.js
+++ lesson_10/task4/index.js
@@ -1,12 +1,12 @@
 const getMaxAbsoluteNumber = arr => {
     let absoluteValue = arr;
     if (!Array.isArray(arr) || absoluteValue.length === 0)
-    return null;
-        arr.map(num => Math.abs(num));
+        return null;
+    absoluteValue.map(num => Math.abs(num));
     return Math.max(...absoluteValue);
 }
 
-const resultArr = [1, 44, 6, 78, 109];
+const resultArr = [-22, 1, 44, 2020, 6, 78, 109, 1999];
 // const resultArr = [];
 
 console.log(getMaxAbsoluteNumber(resultArr));
