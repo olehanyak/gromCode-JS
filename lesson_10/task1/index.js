7246b81c85aab0cc3eebfe6d2ba55ff58dca8cdc
Oleh Hanyak <ohanyak@gmail.com>
2020-03-09 10:49:26 +0200

    num

--- lesson_10/task1/index.js
+++ lesson_10/task1/index.js
@@ -1,17 +1,17 @@
 const getFiniteNumbers = arr => arr
-    .map(elem => Number.isFinite(elem));
+    .filter(elem => Number.isFinite(elem));
 
 const getFiniteNumbersV2 = arr => arr
-    .map(elem => isFinite(elem));
+    .filter(elem => isFinite(elem));
 
 const getNaN = arr => arr
-    .map(elem => Number.isNaN(elem));
+    .filter(elem => Number.isNaN(elem));
 
 const getNaNV2 = arr => arr
-    .map(elem => isNaN(elem));
+    .filter(elem => isNaN(elem));
 
 const getIntegers = arr => arr
-    .map(elem => Number.isInteger(elem));
+    .filter(elem => Number.isInteger(elem));
 
 
 const numberList = [77, '39', '45.9txt', Infinity, null, NaN, undefined];
