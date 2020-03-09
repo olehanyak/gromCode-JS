7bde554d299421eb2b71d8097964bab78cf02180
Oleh Hanyak <ohanyak@gmail.com>
2020-03-09 19:18:09 +0200

    up

--- lesson_10/task3/index.js
+++ lesson_10/task3/index.js
@@ -1,11 +1,10 @@
 const multiRound = num => [
-    Math.floor(num),
-    Math.round(num),
-    Math.ceil(num),
-    Math.trunc(num),
-    // num.toFixed(2),
+    Math.floor(num * 100) / 100,
+    Math.round(num * 100) / 100,
+    Math.ceil(num * 100) / 100,
+    Math.trunc(num * 100) / 100,
+    num.toFixed(2),
 ];
+// const numberArr = [Math.PI];
 
-const numberArr = [14.2];
-
-console.log(multiRound(numberArr));
+console.log(multiRound(Math.PI));
