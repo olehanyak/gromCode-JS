c256b107990f9e18f1b2d5144f008b3412623ae5
Oleh Hanyak <ohanyak@gmail.com>
2020-03-10 17:35:46 +0200

    new_solu

--- lesson_10/hw2/index.js
+++ lesson_10/hw2/index.js
@@ -1,5 +1,9 @@
-const getRandomNumbers = (len, start, end) =>
-   Array(len).fill().map(len => Math.floor(Math.random() * (start - end + 1) + end))
-   // Array(len).map(len => Math.floor(Math.random() * (start - end + 1) + end))
+const getRandomNumbers = (len, start, end) => {
+   if (Math.floor(start) >= Math.floor(end))
+      return null;
+   let randomNum = Array(len).fill().map(len => Math.floor(Math.random() * (start - end + 1) + end))
+   // len.map(len => Math.floor(Math.random() * (start - end + 1) + end))
+   return randomNum;
+}
 
 console.log(getRandomNumbers(10, 1, 7));
