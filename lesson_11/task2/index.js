b65a11e95e41401ba456ccdcb6fe5b46df86fa0b
Oleh Hanyak <ohanyak@gmail.com>
2020-03-11 06:50:22 +0200

    add

---
+++ lesson_11/task2/index.js
@@ -0,0 +1,34 @@
+const contacts = [
+    {
+        name: "William",
+        phoneNumber: "555-55-55"
+    },
+    {
+        name: "Kate",
+        phoneNumber: "444-44-44"
+    },
+    {
+        name: "Sam",
+        phoneNumber: "333-33-33"
+    },
+    {
+        name: "Alex",
+        phoneNumber: "222-22-22"
+    },
+    {
+        name: "Linda",
+        phoneNumber: "111-11-11"
+    }
+];
+
+const sortContacts = contacts => {
+    if (!Array.isArray(contacts)) {
+        return null;
+    }
+    const result = contacts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+    return result;
+};
+
+console.log(sortContacts(contacts));
