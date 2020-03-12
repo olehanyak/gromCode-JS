a8d3ab90935c4a999e8b0a630f489804e284e97c
Oleh Hanyak <ohanyak@gmail.com>
2020-03-12 08:18:21 +0200

    up

--- lesson_6/hw12/index.js
+++ lesson_6/hw12/index.js
@@ -1,11 +1,9 @@
 const getSubArray = (arr, num) => {
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] <= num) {
-            result.push(arr[i]);
-        }
+    for (let i = 0; i < num; i++) {
+        result.push(arr[i]);
     }
     return result;
 };
 
-console.log(getSubArray([1, 2, 3, 4, 5, 6], 2));
+console.log(getSubArray([0, 7, 44, 4, 5, 6], 3));
