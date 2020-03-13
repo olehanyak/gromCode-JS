38be69e874ef7e0bef48e07520a925e6ba1762fa
Oleh Hanyak <ohanyak@gmail.com>
2020-03-13 17:21:37 +0200

    add

--- lesson_13/task2/calculator.test.js
+++ lesson_13/task2/calculator.test.js
@@ -1,9 +1,9 @@
-import getSum, { getSquareArray, getOddNumbers } from './calculator.js'
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js'
 
 it ('should get squared numbers', () => {
-    const result = getSquareArray([1, 2, 3, 4]);
+    const result = getSquaredArray([1, 2, 3]);
 
-    expect(result).toEqual([1, 4, 9, 16]);
+    expect(result).toEqual([1, 4, 9]);
 });
 
 it ('should keep odd numbers only', () => {
