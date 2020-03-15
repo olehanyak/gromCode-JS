4b775d7c64d8dc6915b3cbd1bdbb4cf3f6a6463b
Oleh Hanyak <ohanyak@gmail.com>
2020-03-15 12:11:15 +0200

    unit_test3

---
+++ lesson_13/hw2/calculator.js
@@ -0,0 +1,28 @@
+
+export const calc = expression => {
+    if (typeof expression !== 'string') {
+        return null;
+    }
+    
+    let [a, operation, b] = expression.split(' ');
+    let result;
+    a = +a;
+    b = +b;
+
+    switch (operation) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+
+    return `${expression} = ${result}`; 
+};
