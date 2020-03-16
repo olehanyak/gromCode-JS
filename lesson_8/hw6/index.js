cd79d85942bf23ebadd51c13c2cd6d4d26b6b890
Oleh Hanyak <ohanyak@gmail.com>
2020-03-16 16:10:27 +0200

    up

--- lesson_8/hw6/index.js
+++ lesson_8/hw6/index.js
@@ -1,28 +1,28 @@
 const addPropertyV1 = (userData, userId) => {
-    userData.Id = userId;
+    userData.id = userId;
     return userData;
-}
+};
 
 const addPropertyV2 = (userData, userId) => {
-    const result = Object.assign(userData)
+    const result = Object.assign(userData, {id: userId});
     return result;
-}
+};
 
 const addPropertyV3 = (userData, userId) => {
-    const result = Object.assign({}, userData)
+    const result = Object.assign({}, userData,  {id: userId});
     return result;
-}
+};
 
 const addPropertyV4 = (userData, userId) => {
-    const result = Object.assign({...userData}, )
+    const result = Object.assign({...userData}, {id: userId});
     return result;
-}
+};
 
 const user = {
     name: 'King',
 };
 
-console.log(addPropertyV1(user, '0987654321'))
-console.log(addPropertyV2(user, '0987654321'))
-console.log(addPropertyV3(user, '0987654321'))
-console.log(addPropertyV4(user, '0987654321'))
+// console.log(addPropertyV1(user, '0987654321'))
+// console.log(addPropertyV2(user, '0987654321'))
+// console.log(addPropertyV3(user, '0987654321'))
+// console.log(addPropertyV4(user, '0987654321'))
