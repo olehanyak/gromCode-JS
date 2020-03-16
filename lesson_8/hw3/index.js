f4da68a5d5b9939257c467cb7678197f2691c37d
Oleh Hanyak <ohanyak@gmail.com>
2020-03-16 08:51:05 +0200

    add

---
+++ lesson_8/hw3/index.js
@@ -0,0 +1,30 @@
+const transaction = {
+    currency: 'USD',
+    value: 170,
+    operation: 'sale',
+    agent: {
+      country: 'Ukraine',
+      company: 'NYSE'
+    },
+    'operation time': 1584029990001
+  };
+  
+  /* выведите в консоль значение свойства 'currency' с помощью точки */
+  
+  /* выведите в консоль значение свойства 'value' с помощью квадратных скобок */
+  
+  const key = 'operation';
+  /* выведите в консоль значение свойства, название которого находится в переменной key - используйте квадратные скобки */
+  
+  /* выведите в консоль значение свойства 'company' используя квадратные скобки */
+  
+  /* выведите в консоль значение свойства 'country' используя точку */
+  
+  /* выведите в консоль значение свойства 'operation time' используя квадратные скобки */
+
+  console.log(transaction.currency);
+  console.log(transaction['value']);
+  console.log([key]);
+  console.log(transaction.agent['company']);
+  console.log(transaction.agent.country);
+  console.log(transaction['operation time']);
