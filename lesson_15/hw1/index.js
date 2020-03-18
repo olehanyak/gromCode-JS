8ad5ca2a7076ef652e0346b46092e0c59562e70c
Oleh Hanyak <ohanyak@gmail.com>
2020-03-18 16:51:04 +0200

    add

---
+++ lesson_15/hw1/index.js
@@ -0,0 +1,37 @@
+ export const createCalculator = () => {
+    let memory = 0;
+
+    function add(num) {
+        // console.log(memory += num);
+        return memory += num;
+    };
+
+    function decrease(num) {
+        // console.log(memory -= num);
+        memory -= num;
+    };
+
+    function reset(num) {
+        // console.log(memory = 0);
+        memory = 0;
+    };
+
+    function getMemo(num) {
+        // console.log(memory);
+        return memory;
+    };
+
+    return {
+        add,
+        decrease,
+        reset,
+        getMemo,
+    }
+};
+
+// const calculator1 = createCalculator();
+// calculator1.add(9);
+// calculator1.decrease(9);
+// calculator1.reset(9);
+// calculator1.getMemo(9);
+
