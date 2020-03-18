820d487d48be8c109d986ad2335dd2cdb208f4b7
Oleh Hanyak <ohanyak@gmail.com>
2020-03-18 07:07:12 +0200

    func

---
+++ lesson_14/task1/index.js
@@ -0,0 +1,8 @@
+const message = 'Just learn it!';
+
+const sendMessage = (name) => {
+    const sender = 'Gromcode!';
+    console.log(`${name}, ${message}. Your ${sender}`);
+};
+
+sendMessage('Ann');
