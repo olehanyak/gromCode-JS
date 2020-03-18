b9432223cbaaa76823f658558789e4c934f576e5
Oleh Hanyak <ohanyak@gmail.com>
2020-03-18 10:09:24 +0200

    add

--- lesson_14/hw1/index.js
+++ lesson_14/hw1/index.js
@@ -1,22 +1,22 @@
 let memory = 0;
 
 export function add(num) {
-    return memory += num;
+    memory += num;
 };
 
 export function decrease(num) {
-    return memory -= num;
+    memory -= num;
 };
 
-export function reset() {
-    return 0;
+export function reset(num) {
+    memory = 0;
 };
 
 export function getMemo(num) {
-    return num;
+    return memory;
 };
 
-console.log(add(4));
-console.log(decrease(4));
-console.log(reset(4));
-console.log(getMemo(4));
+// console.log(add(4));
+// console.log(decrease(4));
+// console.log(reset(4));
+// console.log(getMemo(4));
