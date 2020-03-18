a29b8cd3eb6c5086d114494ef383a1f33eba717c
Oleh Hanyak <ohanyak@gmail.com>
2020-03-18 08:35:53 +0200

    add

--- lesson_14/task1/index.js
+++ lesson_14/task1/index.js
@@ -1,8 +1,18 @@
-const message = 'Just learn it!';
+'use strict';
+
+let message = 'Just learn it!';
 
 const sendMessage = (name) => {
     const sender = 'Gromcode!';
     console.log(`${name}, ${message}. Your ${sender}`);
 };
+
+function setMessage(text) {
+    message = text;
+};
+
+sendMessage('Ann');
+
+setMessage('Good job');
 
 sendMessage('Ann');
---
+++ lesson_14/task2/index.js
@@ -0,0 +1,40 @@
+
+
+const createMessenger = () => {
+
+    let message = 'Just learn it!';
+
+    let sender = 'Gromcode!';
+
+    function sendMessage(name) {
+        
+        console.log(`${name}, ${message}. Your ${sender}`);
+    };
+
+    function setMessage(text) {
+        message = text;
+    };
+
+    function setSender(text) {
+        sender = text;
+    };
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+}
+
+// const messenger1 = createMessenger();
+
+// const messenger2 = createMessenger();
+
+// messenger2.setMessage('Good job!');
+
+// messenger1.sendMessage('Bob');
+
+// const messenger3 = createMessenger();
+
+// messenger3.setSender('fontWidth = 700');
+
