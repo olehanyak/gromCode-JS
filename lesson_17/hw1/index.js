9c6f86f9ecf6c640390eb137db47368e6a7c492d
Oleh Hanyak <ohanyak@gmail.com>
2020-03-23 08:30:39 +0200

    up

--- lesson_17/hw1/index.js
+++ lesson_17/hw1/index.js
@@ -2,13 +2,22 @@ const user = {
     firstName: "Roland",
     lastName: "Diskein",
     getFullName() {
-        return (`${this.firstName} ${this.lastName}`);
+        return `${this.firstName} ${this.lastName}`;
         // console.log(`${this.firstName} ${this.lastName}`);
     },
     setFullName(getFullName) {
-        console.log(user.firstName);
-        user.firstName = this.firstName;
-        user.lastName = this.lastName;
+        // console.log(user.firstName);
+        let a = this.getFullName().split(" ");
+        console.log(a[0]);
+
+        // let a = getFullName.forEach(element => {
+        //     console.log(element);
+        // });
+
+        console.log(a[0]);
+        
+        console.log((this.firstName = a[0]));
+        console.log((this.lastName = a[1]));
     }
 };
 
@@ -17,5 +26,3 @@ user.getFullName();
 user.setFullName();
 
 export { user };
-
-
