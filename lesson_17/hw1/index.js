d5e0bd065714d20820fa43394422dacf208bd649
Oleh Hanyak <ohanyak@gmail.com>
2020-03-23 08:48:50 +0200

    up

--- lesson_17/hw1/index.js
+++ lesson_17/hw1/index.js
@@ -13,12 +13,12 @@ const user = {
         // let a = getFullName.forEach(element => {
         //     console.log(element);
         // });
-
-        console.log(a[0]);
-        
-        console.log((this.firstName = a[0]));
-        console.log((this.lastName = a[1]));
-    }
+        this.firstName = a[0];
+        this.lastName = a[1];
+    
+        // console.log((this.firstName = a[0]));
+        // console.log((this.lastName = a[1]));
+    },
 };
 
 user.getFullName();
