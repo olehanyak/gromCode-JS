35b2238b5b37b9906457729b5810129ab9b2ab0e
Oleh Hanyak <ohanyak@gmail.com>
2020-03-24 17:46:40 +0200

    up

--- lesson_18/task1/index.js
+++ lesson_18/task1/index.js
@@ -6,12 +6,13 @@ const event = {
         { name: 'Bob', email: 'example@Server.com', age: 18 },
         { name: 'Sam', email: 'example@Server.com', age: 22 },
     ],
-    message: 'Welcome to the party',
+    message: "Welcome to the party!",
     getInvintations() {
         return this.guests
         .filter(({ age }) => age >= 18)
         .map(({ name, email }) => ({
-            text: `Hi ${name}. ${this.message}`,
+            email: `${email} `,
+            message: `Dear ${name}! ${this.message}`,
             email
         }));
     }
