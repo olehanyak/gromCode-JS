76d2968498d8ddd6ad2cf239042d2bbf9210d2b8
Oleh Hanyak <ohanyak@gmail.com>
2020-03-24 15:19:09 +0200

    add

--- lesson_17/hw3/index.js
+++ lesson_17/hw3/index.js
@@ -1,23 +1,27 @@
-const person = {
-    firstName: 'John',
-    lastName: 'Rambo',
-    hobby: 'war',
-};
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Rambo',
+//     hobby: 'war',
+// };
 
-function fun() {
-    console.log(`Hello, mr.${this.firstName} ${this.lastName}`);
-};
+// function fun() {
+//     console.log(`Hello, mr.${this.firstName} ${this.lastName}`);
+// };
 
-function bind(func, context) {
-    let rest = Array.prototype.slice.call(arguments);
-    // console.log(rest)
-    // console.log(Array.prototype)
-    return function () {
-        return func.apply(context, rest);
+// function bind(func, context) {
+//     let rest = Array.prototype.slice.call(arguments);
+//     // console.log(rest)
+//     // console.log(Array.prototype)
+//     return function () {
+//         return func.apply(context, rest);
+//     }
+// };
+
+export const bind = (func, context, ...args) => {
+    return function(...targetArgs) {
+        return func.call(context, ...args, ...targetArgs);
     }
-};
+}
 
-bind(fun, person)();
-bind(fun, person)();
-
-export { bind };
+// bind(fun, person)();
+// bind(fun, person)();
