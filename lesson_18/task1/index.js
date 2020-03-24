78056439fb988c079f28b42c0b115941d650b968
Oleh Hanyak <ohanyak@gmail.com>
2020-03-24 17:28:15 +0200

    add

---
+++ lesson_18/task1/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const event = {
+    guests: [
+        { name: 'Tom', email: 'example@Server.com', age: 17 },
+        { name: 'Bob', email: 'example@Server.com', age: 18 },
+        { name: 'Sam', email: 'example@Server.com', age: 22 },
+    ],
+    message: 'Welcome to the party',
+    getInvintations() {
+        return this.guests
+        .filter(({ age }) => age >= 18)
+        .map(({ name, email }) => ({
+            text: `Hi ${name}. ${this.message}`,
+            email
+        }));
+    }
+}
+
+console.log(event.getInvintations());
+
+export { event };
