c10498c883e96e118a8d565fec7b300ccdee203f
Oleh Hanyak <ohanyak@gmail.com>
2020-03-26 13:02:54 +0200

    up

--- lesson_19/task4/index.js
+++ lesson_19/task4/index.js
@@ -2,34 +2,34 @@ function User(name, age) {
     this.name = name;
     this.age = age;
 };
-User.prototype.sayHi = () => {
+
+User.prototype.sayHi = function() {
     console.log(`Hi, I am ${this.name}`);
-}
-User.prototype.requestNewPhoto = () => {
+};
+
+User.prototype.requestNewPhoto = function() {
     console.log(`New photo request was sent for ${this.name}`);
-}
-User.prototype.setAge = (age) => {
-  
+};
+
+User.prototype.setAge = function(age) {
     this.age = age;
-    console.log(this.age)
-    if (this.age >= 25) {
+    console.log(age);
+    if (age >= 25) {
         console.log(`New photo request was sent for ${this.name}`);
         // this.age = age;
     }
-    if (this.age < 0) {
+    if (age < 0) {
         return false;
     }
 
-    return this.age;
-
+    return age;
 };
 
-const user1 = new User('Olaf', 3);
-console.log(user1)
+const user1 = new User("Olaf", 3);
+console.log(user1);
 
 // user1.sayHi()
 // user1.setAge()
 // user1.requestNewPhoto()
 
 export { User };
-
