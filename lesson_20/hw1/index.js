52b59d224e99f9a71f532dc39f5a4acc6f648a51
Oleh Hanyak <ohanyak@gmail.com>
2020-03-27 15:38:35 +0200

    add

---
+++ lesson_20/hw1/index.js
@@ -0,0 +1,36 @@
+class Order {
+    constructor(price, city, type) {
+        this.id = Math.random();
+        this.price = price;
+        this.dateCreated = new Date();
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date();
+        this.city = city;
+        this.type = type;
+    }
+
+    checkPrice() {
+        if (this.price < 1000) return false;
+        if (this.price > 1000) return true;
+    }
+    confirmOrder() {
+        if (this.isConfirmed === true) {
+            return this.dateConfirmed = this.dateCreated;
+        }
+    }
+
+    isValidType() {
+        if (this.type === 'Buy' || this.type === 'Sell') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+const newOrder = new Order(33, 'Lion', 'Sell');
+
+console.log(newOrder)
+console.log(newOrder.checkPrice());
+console.log(newOrder.confirmOrder());
+console.log(newOrder.isValidType());
