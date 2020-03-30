172c9dfab73a677aaf60614a1f7413e4f71e28e7
Oleh Hanyak <ohanyak@gmail.com>
2020-03-30 19:19:30 +0300

    add

---
+++ lesson_21/hw9/index.js
@@ -0,0 +1,18 @@
+function finishForm() {
+    const setLogin = document.querySelector('input');
+    setLogin.setAttribute('name', 'login')
+
+    const newInput = document.createElement('input');
+
+    const setNewInput = document.querySelector('.login-form');
+    setNewInput.append(newInput);
+   
+    newInput.setAttribute('type', 'password');
+    newInput.setAttribute('name', 'password');
+    // console.log(setNewInput)
+    // console.log(setLogin)
+};
+
+// finishForm();
+
+export { finishForm };
