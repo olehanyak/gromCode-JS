eec20e3f544c3f2a4edd9a37e201c385eb35a18a
Oleh Hanyak <ohanyak@gmail.com>
2020-03-30 13:41:28 +0300

    add

--- lesson_21/hw3/index.js
+++ lesson_21/hw3/index.js
@@ -1,31 +1,33 @@
-export function getTitle() {
+function getTitle() {
     const getContent = document.querySelector('.title');
-    console.log(getContent.textContent);
+    // console.log(getContent.textContent);
     return getContent.textContent;
 };
 
 getTitle();
 
-export function getDescription() {
+function getDescription() {
     const getContent = document.querySelector('.about');
-    console.log(getContent.innerText);
+    // console.log(getContent.innerText);
     return getContent.innerText;
 };
 
 getDescription();
 
-export function getPlans() {
+function getPlans() {
     const getContent = document.querySelector('.plans');
-    console.log(getContent.innerHTML);
+    // console.log(getContent.innerHTML);
     return getContent.innerHTML;
 };
 
 getPlans();
 
-export function getGoal() {
+function getGoal() {
     const getContent = document.querySelector('.goal');
-    console.log(getContent.outerHTML);
+    // console.log(getContent.outerHTML);
     return getContent.outerHTML;
 };
 
 getGoal();
+
+export { getTitle, getDescription, getPlans, getGoal };
