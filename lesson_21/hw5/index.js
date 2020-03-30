f4067b463e674bae49f886fc76486a68e0da69b4
Oleh Hanyak <ohanyak@gmail.com>
2020-03-30 14:53:26 +0300

    add

---
+++ lesson_21/hw5/index.js
@@ -0,0 +1,11 @@
+function setButton(buttonText) {
+    const setText = document.querySelector('body');
+    setText.innerHTML = buttonText;
+    // setText.textContent = buttonText;
+    // console.log(buttonText)
+    // console.log(setText)
+};
+
+setButton('<button>button text</button>');
+
+export { setButton };
