ca6fc57e2472ee3bd7d81516904924c1b87415cd
Oleh Hanyak <ohanyak@gmail.com>
2020-04-01 09:18:47 +0300

    add

---
+++ lesson_22/hw6/index.js
@@ -0,0 +1,11 @@
+
+function hendleClick(event) {
+    const getText = event.target;
+    console.log(getText.textContent);
+};
+
+const setText = document.querySelectorAll('.btn');
+
+setText.forEach(function(btn) {
+    btn.addEventListener('click', hendleClick);
+});
