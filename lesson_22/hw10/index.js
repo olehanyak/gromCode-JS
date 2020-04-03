aa03a9d100dcfe0860a903081fa7e045422fd957
Oleh Hanyak <ohanyak@gmail.com>
2020-04-03 08:52:51 +0300

    add

--- lesson_22/hw10/index.js
+++ lesson_22/hw10/index.js
@@ -1,71 +1,102 @@
-const tasks = [{
-        text: 'Buy milk',
+const tasks = [
+    {
+        text: "Buy milk",
         done: false
     },
     {
-        text: 'Pick up Tom from airport',
+        text: "Pick up Tom from airport",
         done: false
     },
     {
-        text: 'Visit party',
+        text: "Visit party",
         done: false
     },
     {
-        text: 'Visit doctor',
+        text: "Visit doctor",
         done: true
     },
     {
-        text: 'Buy meat',
+        text: "Buy meat",
         done: true
-    },
+    }
 ];
 
 const renderListItem = listItems => {
-    const listElement = document.querySelector('.list');
-    listElement.innerHTML = '';
+    const listElement = document.querySelector(".list");
+    listElement.innerHTML = "";
 
     const listItemsElements = listItems
         .sort((a, b) => a.done - b.done)
-        .map(({
-            text,
-            done
-        }) => {
-            const listItemElement = document.createElement('li');
-            listItemElement.classList.add('list__item');
+        .map(({ text, done }) => {
+            const listItemElement = document.createElement("li");
+            listItemElement.classList.add("list__item");
             if (done) {
-                listItemElement.classList.add('list__item_done');
+                listItemElement.classList.add("list__item_done");
             }
-            const checkboxElement = document.createElement('input');
-            checkboxElement.setAttribute('type', 'checkbox');
+            const checkboxElement = document.createElement("input");
+            checkboxElement.setAttribute("type", "checkbox");
             checkboxElement.checked = done;
-            checkboxElement.classList.add('list__item-checkbox');
+            checkboxElement.classList.add("list__item-checkbox");
             listItemElement.append(checkboxElement, text);
 
             return listItemElement;
-        })
+        });
     listElement.append(...listItemsElements);
 };
 
+// --------------------------------------------------------------------------
 
-const getValueInput = document.querySelector('.task-input');
+const getValueInput = document.querySelector(".task-input");
 // console.log(getValueInput.value)
 
-const btn = document.querySelector('.create-task-btn');
+const btn = document.querySelector(".create-task-btn");
 
-function create() {
+function createNewTask() {
     tasks.push({
         text: getValueInput.value,
-        done: false,
+        done: false
     });
-    // const newList = document.createElement('li');
-    // newList.classList.add('list__item');
 
-    if (getValueInput.value === '') return false;
-    getValueInput.value = '';
+    if (getValueInput.value === "") return false;
+    getValueInput.value = "";
 
     return renderListItem(tasks);
 };
 
-btn.addEventListener('click', create);
+btn.addEventListener("click", createNewTask);
+
+const choiceList = document.querySelector(".list");
+
+// function check() {
+//     const a = document.querySelectorAll(".list__item-checkbox");
+//     a.forEach(elem => {
+//         tasks.map(function(item) {
+//             if (elem.checked) {
+//                 item.done = true;
+//                 console.log(item.done);
+//             }
+//             console.log(item.done);
+//         });
+
+//         console.log(elem);
+//     });
+//     renderListItem(tasks);
+// };
+
+// check();
+
+
+function check (e) {
+    const similar = tasks.find(item => item.text === e.target.parentNode.textContent);
+    console.log(e.target.parentNode.textContent);
+    similar.done = e.target.checked;
+    console.log(e.target.checked);
+    console.log(similar.done);
+
+    renderListItem(tasks);
+};
+
+choiceList.addEventListener("click", check);
 
 renderListItem(tasks);
+// ===============================================
