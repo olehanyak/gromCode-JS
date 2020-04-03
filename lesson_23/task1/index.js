f007aead967998f1889f21a6224bca72ed68f3db
Oleh Hanyak <ohanyak@gmail.com>
2020-04-03 14:01:18 +0300

    add

--- lesson_23/task1/index.js
+++ lesson_23/task1/index.js
@@ -18,7 +18,7 @@ const emailInputElem = document.querySelector('#email');
 const passwordInputElem = document.querySelector('#password');
 
 const emailErrorElem = document.querySelector('.error-text_email');
-const passwordErrorElem = document.querySelector('#error-text_password');
+const passwordErrorElem = document.querySelector('.error-text_password');
 
 const isRequired = value => value ?
     undefined :
