a91b73cc0d9e8bbf346e191636a1b28763bbb7db
Oleh Hanyak <ohanyak@gmail.com>
2020-04-07 07:39:02 +0300

    add

--- lesson_24/hw1/index.js
+++ lesson_24/hw1/index.js
@@ -1,11 +1,36 @@
 
 
-const getDiff = (startDate, endDate) => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const newDate = new Date(start - end);
-    console.log(start);
-    console.log(end);
+// const getDiff = (startDate, endDate) => {
+//     const start = new Date(startDate);
+//     const end = new Date(endDate);
+//     const newDate = new Date(start - end);
+//     console.log(start);
+//     console.log(end);
+
+//     let newDay = newDate.getDate();
+//     let newHour = newDate.getHours();
+//     let newMin = newDate.getMinutes();
+//     let newSec = newDate.getSeconds();
+
+//     console.log(`${newDay}d ${newHour}h ${newMin}m ${newSec}s`);
+//     return `${newDay}d ${newHour}h ${newMin}m ${newSec}s`;
+// };
+
+// const startDate = new Date();
+// const endDate = new Date(2010, 10, 10, 10, 10, 10);
+
+// const myDate = getDiff(startDate, endDate);
+
+// console.log(myDate);
+
+// export { getDiff };
+function getDiff(startDate, endDate) {
+
+    const start = Math.floor(startDate.getTime() / (3600 * 24 * 1000));
+    const end = Math.floor(endDate.getTime() / (3600 * 24 * 1000));
+
+    const newDate = new Date(end - start);
+
 
     let newDay = newDate.getDate();
     let newHour = newDate.getHours();
@@ -16,11 +41,9 @@ const getDiff = (startDate, endDate) => {
     return `${newDay}d ${newHour}h ${newMin}m ${newSec}s`;
 };
 
-const startDate = new Date(2000, 5, 10, 10, 10);
-const endDate = new Date(2010, 10, 10, 10, 10, 10);
+const startDate = new Date(2013, 11, 4, 0, 0, 0);
+const endDate = new Date(2013, 13, 4, 1, 1, 1);
 
-const myDate = getDiff(startDate, endDate);
-
-console.log(myDate);
+getDiff(startDate, endDate)
 
 export { getDiff };
