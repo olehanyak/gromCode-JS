cbdf690e9ccea3ebda86d69d2c6cc34294a53e8f
Oleh Hanyak <ohanyak@gmail.com>
2020-04-09 20:30:30 +0300

    add

--- lesson_28/task1/index.js
+++ lesson_28/task1/index.js
@@ -6,7 +6,7 @@ export const calc = initValue => {
             result += value;
             return calculator;
         },
-        substract(value) {
+        subtract(value) {
             result -= value;
             return this;
         },
@@ -26,6 +26,6 @@ export const calc = initValue => {
     return calculator;
 };
 
-// const result = calc(3).add(2).mult(4).div(10).substract(5).result();
+// const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
 
 // console.log(result);
