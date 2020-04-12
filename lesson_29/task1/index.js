136f0cf6ebd89d36bd88eeb3d6bef4be745ff3c8
Oleh Hanyak <ohanyak@gmail.com>
2020-04-12 18:10:16 +0300

    add

--- lesson_29/task1/index.js
+++ lesson_29/task1/index.js
@@ -8,7 +8,7 @@ const addImage = (imgSrc, callback) => {
     const onImageLoaded = () => {
         const { width, height } = imgElem;
         console.log(width, height);
-        callback(null, { width, height });
+        callback(null, { width: 200, height: 100 });
     };
 
     imgElem.addEventListener('load', onImageLoaded);
@@ -16,7 +16,7 @@ const addImage = (imgSrc, callback) => {
     imgElem.addEventListener('error', () => callback('Image load failed'));
 };
 
-const imgSrc = 'https://envato-shoebox-0.imgix.net/1846/dffa-0fdc-440e-ab21-c98111875abe/4H1A1612.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=800&s=4e1e92f33087336e76504c86758a5c0e';
+const imgSrc = 'https://server.com/image.png';
 
 const onImageLoaded = (error, data) => {
     if (error) {
