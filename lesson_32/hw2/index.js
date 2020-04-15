fa873a96b4cd365ce4b4042f48c74f9e7d12d031
Oleh Hanyak <ohanyak@gmail.com>
2020-04-15 18:15:52 +0300

    add

---
+++ lesson_32/hw2/index.js
@@ -0,0 +1,22 @@
+const promiseNumber1 = Promise.resolve(67);
+const promiseNumber2 = Promise.resolve(23);
+const promiseNumber3 = Promise.resolve(8);
+
+/*
+ * создайте промис и присвойте переменной resultPromise
+ * чтобы в консоль вывелась сумма всех чисел из трех промисов
+ */
+
+const resultPromise = Promise.all([promiseNumber1, promiseNumber2, promiseNumber3])
+        .then(result => result)
+
+resultPromise
+    .then(numbersList => {
+        console.log(numbersList);
+        const sum = numbersList.reduce((acc, num) => acc + num, 0);
+        return sum;
+    })
+    .then(result => {
+        console.log(result); // 98
+    });
+
