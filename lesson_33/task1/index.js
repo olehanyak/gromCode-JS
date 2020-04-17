fc367ab2824c2ccca17f83f15acb7d7e8944e673
Oleh Hanyak <ohanyak@gmail.com>
2020-04-17 08:37:27 +0300

    add

--- lesson_33/task1/index.js
+++ lesson_33/task1/index.js
@@ -2,7 +2,8 @@ const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
 
-const defaultAvatar = 'https:avatars3.githubusercontent.com/u10001';
+// const defaultAvatar = 'https:avatars3.githubusercontent.com/u10001';
+const defaultAvatar = 'https:api.github.com/users/USERNAME';
 
 userAvatarElem.src = defaultAvatar;
 
